18ff7e3703678b6eab78697acdd42991bb2bbb20
vikh1976 <84524106+vikh1976@users.noreply.github.com>
2021-08-02 18:52:35 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,4 @@
 import telebot
-import requests
 
 from config import *
 from extensions import APIException, Converter
@@ -7,6 +6,7 @@ from extensions import APIException, Converter
 bot = telebot.TeleBot(TOKEN)
 
 
+# Вывод инструкции по использованию бота
 @bot.message_handler(commands=['start', 'help'])
 def help_msg(message: telebot.types.Message):
     text = 'Чтобы начать работу введите команду боту в следующем формате:\n<Имя валюты> ' \
@@ -16,6 +16,7 @@ def help_msg(message: telebot.types.Message):
     bot.reply_to(message, text)
 
 
+# Вывод списка допустимых валют
 @bot.message_handler(commands=['values'])
 def values(message: telebot.types.Message):
     text = 'Доступные валюты:'
@@ -24,19 +25,25 @@ def values(message: telebot.types.Message):
     bot.reply_to(message, text)
 
 
+# Основная функция обработки ввода пользователя
 @bot.message_handler(content_types=['text', ])
 def convert(message: telebot.types.Message):
     try:
+        # Если введено менее или более трех параметров - сообщение об ошибке
         mes = message.text.split()
         if len(mes) != 3:
             raise APIException('Неверное число параметров')
         quote, base, amount = mes
+        # Получаем цену выбранной валюты
         total_base = (Converter.get_price(quote, base, amount))
     except APIException as e:
+        # В случае ошибок ввода выводим соотв. сообщение
         bot.reply_to(message, f'Ошибка ввода:\n{e}')
     except Exception as e:
+        # При любых других ошибках показываем эту ошибку
         bot.reply_to(message, f'Не удалось обработать команду\n{e}')
     else:
+        # Если все в порядке - выводим результат работы
         text = f'Цена {amount} {quote} в {base} - {total_base:.2f}'
         bot.send_message(message.chat.id, text)
 
